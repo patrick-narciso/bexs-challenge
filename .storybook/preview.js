453b86205c232aa747a3666372d4822dffc37eb6
Patrick Narciso <patrick.ncsilva@gmail.com>
2020-12-16 21:56:02 -0300

    chore(storybook): add theme and font support in storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+import theme from '../src/commons/styled-components/theme';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+export const decorators = [
+	(Story) => (
+		<ThemeProvider theme={theme}>
+			<Story />
+		</ThemeProvider>
+	),
+];
